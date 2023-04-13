47b9b56209de7adb50f0a6fef499e6868be5ee21
Russ Webber <russ.webber@greenroomrobotics.com>
2023-04-13 16:22:35 +1000

    chore: add templated test

--- test/launch_ext/test_templated.py
+++ test/launch_ext/test_templated.py
@@ -2,15 +2,18 @@
 from launch import LaunchContext
 from launch_ext.substitutions import Templated
 from launch.actions import SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 
 def test_file_contents():
     lc = LaunchContext()
 
     SetLaunchConfiguration('test_sub', "world").visit(lc)
 
-    sub = Templated(template="""hello $test_sub""")
+    sub = Templated("""hello $test_sub""")
     assert sub.perform(lc) == 'hello world'
 
-
-    sub = Templated(template="""hello ${test_sub}s""")
+    sub = Templated("""hello ${test_sub}s""")
     assert sub.perform(lc) == 'hello worlds'
+
+    SetLaunchConfiguration('test_temp', "this is a template in a sub hello $test_sub").visit(lc)
+    assert Templated(LaunchConfiguration("test_temp")).perform(lc) == "this is a template in a sub hello world"
