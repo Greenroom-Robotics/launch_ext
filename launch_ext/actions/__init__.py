6ea1d34d9f7115c974350fb892f73b3881c14e48
Russ Webber <russ.webber@greenroomrobotics.com>
2023-04-20 17:36:03 +1000

    feat: add WriteFile action

--- launch_ext/actions/__init__.py
+++ launch_ext/actions/__init__.py
@@ -2,8 +2,10 @@
 
 from .log_rotate import LogRotate
 from .include_package_launch_file import IncludePackageLaunchFile
+from .write_file import WriteFile
 
 __all__ = [
     'LogRotate',
     'IncludePackageLaunchFile'
+    'WriteFile'
 ]
---
+++ launch_ext/actions/write_file.py
@@ -0,0 +1,27 @@
+"""Module for the WriteFile action."""
+
+from pathlib import Path
+import launch.logging
+from launch.actions import OpaqueFunction
+from launch.launch_context import LaunchContext
+from launch.some_substitutions_type import SomeSubstitutionsType
+
+
+def write_file(context: LaunchContext, contents: SomeSubstitutionsType, dest_path: SomeSubstitutionsType, overwrite: bool=True):
+    contents = context.perform_substitution(contents)
+    dest_path = Path(context.perform_substitution(dest_path))
+
+    if dest_path.exists() and not overwrite:
+        launch.logging.get_logger('launch.user').warn(f"File {dest_path} exists, not overwriting")
+        return
+
+    dest_path.write_text(contents)
+    launch.logging.get_logger('launch.user').info(f"Wrote file '{dest_path}'.")
+
+
+def WriteFile(contents: SomeSubstitutionsType, dest_path: SomeSubstitutionsType) -> OpaqueFunction:
+    """Action that writes a substituion to a file."""
+    return OpaqueFunction(function=write_file, kwargs={
+        'contents': contents,
+        'dest_path': dest_path,
+    })
