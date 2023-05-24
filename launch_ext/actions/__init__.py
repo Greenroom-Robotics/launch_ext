06662c99a0cbcaa006b70a54ba871b8ae8a4bb33
David Revay <daverevay@gmail.com>
2023-05-24 13:38:35 +1000

    chore: add the thing

--- launch_ext/actions/__init__.py
+++ launch_ext/actions/__init__.py
@@ -3,9 +3,11 @@
 from .log_rotate import LogRotate
 from .include_package_launch_file import IncludePackageLaunchFile
 from .write_file import WriteFile
+from .set_launch_configuration_if_not_none import SetLaunchConfigurationIfNotNone
 
 __all__ = [
     'LogRotate',
     'IncludePackageLaunchFile'
-    'WriteFile'
+    'WriteFile',
+    'SetLaunchConfigurationIfNotNone'
 ]
