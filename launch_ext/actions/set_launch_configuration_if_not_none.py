277fc27de86271a81ef7e169e4c5e89ab5f9ad89
David Revay <MrBlenny@users.noreply.github.com>
2023-05-24 13:43:43 +1000

    feat: add SetLaunchConfigurationIfNotNone
    feat: add an action that sets the launch configuration if not none

---
+++ launch_ext/actions/set_launch_configuration_if_not_none.py
@@ -0,0 +1,11 @@
+import launch
+from launch.actions import SetLaunchConfiguration
+from launch.some_substitutions_type import SomeSubstitutionsType
+
+
+
+def SetLaunchConfigurationIfNotNone(name: str, value: SomeSubstitutionsType | None):
+    """
+    Set a launch configuration if the value is not None.
+    """
+    return SetLaunchConfiguration(name, value) if value is not None else launch.LaunchDescriptionEntity()
